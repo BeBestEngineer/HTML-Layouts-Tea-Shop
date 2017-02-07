fc3e8738ee7f1249edb1b3ab9767a3511bffd1e2
Alexandr Ja <mr-spinn@outlook.com>
2017-02-07 11:56:49 +0600

    replace banner_1 to banners

--- source/other_modules/custom_modules/banner_1/banner_1.php
+++
@@ -1,8 +0,0 @@
-<section class="banner-1">
-  <h2 class="banner-1__title">Tea collection</h2>
-  <span class="banner-1__subtitle">Green tea</span>
-  <p class="banner-1__desc">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cum, debitis dicta eius esse eveniet ex, id ipsam ipsum libero neque nulla, quos recusandae reiciendis rem rerum sed similique unde.
-  </p>
-  <a class="links" href="#">Shop now</a>
-</section>
