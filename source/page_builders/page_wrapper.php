63326d925393e9d648172d7ad848d5e42ead5a90
Alexandr Ja <mr-spinn@outlook.com>
2017-02-10 00:54:52 +0600

    add data in "page wrapper" class

--- source/page_builders/page_wrapper.php
+++ source/page_builders/page_wrapper.php
@@ -1,5 +1,4 @@
 
-    <div class="page-wrapper">
+    <div class="page-wrapper<?php echo $page_data -> Get_data( $_GET['page'] )['suffix']; ?>" id="page-wrapper-id">
     <?php require_once 'page_builders/'. $_GET['page'] .'_page_builder/'. $_GET['page'] .'_page_builder.php'; ?>
-    </div>
-
+    </div>  
