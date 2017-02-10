24c48deabb79ce767473537d4a766b48e5574793
Alexandr Ja <mr-spinn@outlook.com>
2017-02-10 21:43:47 +0600

    ready grid system for header-1

--- source/other_modules/headers/header_1/header_1.php
+++ source/other_modules/headers/header_1/header_1.php
@@ -1,7 +1,6 @@
 <header class="header-1">
   <nav class="navbar  header-1__navbar">
     <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
         <button type="button" class="mobile-menu-button  navbar-toggle collapsed" data-toggle="collapse" data-target="#header-1__navbar-collapse-id" aria-expanded="false">
           <span class="sr-only">Toggle navigation</span>
@@ -9,13 +8,14 @@
           <span class="icon-bar  mobile-menu-button__icon-bar"></span>
           <span class="icon-bar  mobile-menu-button__icon-bar"></span>
         </button>
-<!--        <a class="navbar-brand" href="#">Teashop logo</a>-->
       </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="header-1__navbar-collapse-id">
+
         <div class="row">
-          <ul class="nav navbar-nav                header-1__row--top">
+          <div class="row">
+            <div class="col-sm-8">
+              <ul class="nav navbar-nav                header-1__row--top">
           <li>
             <a class="links" href="#">
               <svg class="allign-middle  top-nav__icon" width="20" height="20" viewBox="0 0 1792 1792">
@@ -66,8 +66,9 @@
             </a>
           </li>
         </ul>
-
-          <ul class="nav navbar-nav  navbar-right  header-1__currencys">
+            </div>
+            <div class="col-sm-4">
+              <ul class="nav navbar-nav  navbar-right  header-1__currencys">
             <li class="dropdown">
               <a class="links" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">English <span class="caret"></span></a>
               <ul class="dropdown-menu">
@@ -83,6 +84,8 @@
               </ul>
             </li>
           </ul>
+            </div>
+          </div>
         </div>
 
 
@@ -96,7 +99,9 @@
 
 
         <div class="row">
-          <ul class="nav navbar-nav                header-1__row--bottom">
+          <div class="row">
+            <div class="col-sm-10">
+              <ul class="nav navbar-nav                header-1__row--bottom">
             <li>
               <a class="links" href="#">
                 <span class="allign-middle">Home</span>
@@ -152,88 +157,131 @@
               </a>
             </li>
           </ul>
+            </div>
+            <div class="col-sm-2">
+              <ul class="nav navbar-nav  navbar-right  header-1__shopping-basket">
+                <li class="dropdown">
+                  <a class="links" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    <svg width="20" height="20" viewBox="0 0 1792 1792" >
+                      <path class="icons__svg-color" d="M1792 768q53 0 90.5 37.5t37.5 90.5-37.5 90.5-90.5 37.5h-15l-115 662q-8 46-44 76t-82 30h-1280q-46 0-82-30t-44-76l-115-662h-15q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5h1792zm-1435 800q26-2 43.5-22.5t15.5-46.5l-32-416q-2-26-22.5-43.5t-46.5-15.5-43.5 22.5-15.5 46.5l32 416q2 25 20.5 42t43.5 17h5zm411-64v-416q0-26-19-45t-45-19-45 19-19 45v416q0 26 19 45t45 19 45-19 19-45zm384 0v-416q0-26-19-45t-45-19-45 19-19 45v416q0 26 19 45t45 19 45-19 19-45zm352 5l32-416q2-26-15.5-46.5t-43.5-22.5-46.5 15.5-22.5 43.5l-32 416q-2 26 15.5 46.5t43.5 22.5h5q25 0 43.5-17t20.5-42zm-1156-1217l-93 412h-132l101-441q19-88 89-143.5t160-55.5h167q0-26 19-45t45-19h384q26 0 45 19t19 45h167q90 0 160 55.5t89 143.5l101 441h-132l-93-412q-11-44-45.5-72t-79.5-28h-167q0 26-19 45t-45 19h-384q-26 0-45-19t-19-45h-167q-45 0-79.5 28t-45.5 72z" fill="#fff"/>
+                    </svg>
+                    <span class="shopping-basket__badge  badge">4</span>
+                  </a>
+                  <div class="dropdown-menu">
+                    <ul class="  shopping-basket__list">
 
-          <ul class="nav navbar-nav  navbar-right  header-1__shopping-basket">
-            <li class="dropdown">
-              <a class="links" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                <svg width="20" height="20" viewBox="0 0 1792 1792" >
-                  <path class="icons__svg-color" d="M1792 768q53 0 90.5 37.5t37.5 90.5-37.5 90.5-90.5 37.5h-15l-115 662q-8 46-44 76t-82 30h-1280q-46 0-82-30t-44-76l-115-662h-15q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5h1792zm-1435 800q26-2 43.5-22.5t15.5-46.5l-32-416q-2-26-22.5-43.5t-46.5-15.5-43.5 22.5-15.5 46.5l32 416q2 25 20.5 42t43.5 17h5zm411-64v-416q0-26-19-45t-45-19-45 19-19 45v416q0 26 19 45t45 19 45-19 19-45zm384 0v-416q0-26-19-45t-45-19-45 19-19 45v416q0 26 19 45t45 19 45-19 19-45zm352 5l32-416q2-26-15.5-46.5t-43.5-22.5-46.5 15.5-22.5 43.5l-32 416q-2 26 15.5 46.5t43.5 22.5h5q25 0 43.5-17t20.5-42zm-1156-1217l-93 412h-132l101-441q19-88 89-143.5t160-55.5h167q0-26 19-45t45-19h384q26 0 45 19t19 45h167q90 0 160 55.5t89 143.5l101 441h-132l-93-412q-11-44-45.5-72t-79.5-28h-167q0 26-19 45t-45 19h-384q-26 0-45-19t-19-45h-167q-45 0-79.5 28t-45.5 72z" fill="#fff"/>
-                </svg>
-                <span class="shopping-basket__badge  badge">4</span>
-              </a>
-              <ul class="dropdown-menu  shopping-basket__list">
+                    <?php for ($i = 0; $i <= 3; ++$i): ?>
+                      <li class="shopping-basket__item">
+                        <div class="container-fluid">
+                          <div class="row">
 
-                <?php for ($i = 0; $i <= 3; ++$i): ?>
-                  <li class="shopping-basket__item">
-                    <div class="container-fluid">
-                      <div class="row">
-
-                        <div class="col-xs-4">
-                          <a class="links  image-wrapper  shopping-basket__image-wrapper" href="?page=product">
-                            <img class="image  shopping-basket__image" src="images/header-1/shopping-basket-dropdown-menu.png" alt="...">
-                          </a>
-                        </div>
-
-
-                        <div class="col-xs-6">
-                          <div class="shopping-basket__desc-wrapper">
-                            <h3 class="shopping-basket__title">
-                              <a class="links  shopping-basket__link" href="#">Herbal tea</a>
-                            </h3>
-
-                            <div class="rating  rating-4-stars">
-                              <svg width="15" height="15" viewBox="0 0 1792 1792">
-                                <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
-                              </svg>
-                              <svg width="15" height="15" viewBox="0 0 1792 1792">
-                                <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
-                              </svg>
-                              <svg width="15" height="15" viewBox="0 0 1792 1792">
-                                <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
-                              </svg>
-                              <svg width="15" height="15" viewBox="0 0 1792 1792">
-                                <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
-                              </svg>
-                              <svg width="15" height="15" viewBox="0 0 1792 1792">
-                                <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
-                              </svg>
+                            <div class="col-xs-4">
+                              <a class="links  image-wrapper  shopping-basket__image-wrapper" href="?page=product">
+                                <img class="image  shopping-basket__image" src="images/header-1/shopping-basket-dropdown-menu.png" alt="...">
+                              </a>
                             </div>
 
-                          <span class="product-price">
-                            <s class="product-price__without-discont">
-                              <span class="">€</span>
-                              <span class="">500</span>
-                            </s>
-                            <span class="product-price__with-discont">
-                              <span class="">$</span>
-                              <span class="">255</span>
-                            </span>
-                          </span>
+
+                            <div class="col-xs-6">
+                              <div class="shopping-basket__desc-wrapper">
+                                <h3 class="shopping-basket__title">
+                                  <a class="links  shopping-basket__link" href="#">Herbal tea</a>
+                                </h3>
+
+                                <div class="rating  rating-4-stars">
+                                  <svg width="15" height="15" viewBox="0 0 1792 1792">
+                                    <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+                                  </svg>
+                                  <svg width="15" height="15" viewBox="0 0 1792 1792">
+                                    <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+                                  </svg>
+                                  <svg width="15" height="15" viewBox="0 0 1792 1792">
+                                    <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+                                  </svg>
+                                  <svg width="15" height="15" viewBox="0 0 1792 1792">
+                                    <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+                                  </svg>
+                                  <svg width="15" height="15" viewBox="0 0 1792 1792">
+                                    <path class="icons__svg-color" d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+                                  </svg>
+                                </div>
+
+                              <span class="product-price">
+                                <s class="product-price__without-discont">
+                                  <span class="">€</span>
+                                  <span class="">500</span>
+                                </s>
+                                <span class="product-price__with-discont">
+                                  <span class="">$</span>
+                                  <span class="">255</span>
+                                </span>
+                              </span>
+
+                              </div>
+                            </div>
+
+
+                            <div class="col-xs-2  text-right">
+                              <a href="#" class="links  shopping-basket__action--delete">
+                                <svg width="20" height="20" viewBox="0 0 1792 1792">
+                                  <path class="icons__svg-color" d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"></path>
+                                </svg>
+                              </a>
+                            </div>
 
                           </div>
                         </div>
+                      </li>
+                    <? endfor; ?>
 
+                  </ul>
 
-                        <div class="col-xs-2  text-right">
-                          <a href="#" class="links  shopping-basket__action--delete">
-                            <svg width="20" height="20" viewBox="0 0 1792 1792">
-                              <path class="icons__svg-color" d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"></path>
-                            </svg>
-                          </a>
+                    <div class="shopping-basket__footer  ">
+                    <div class="container-fluid">
+
+                      <div class="row  shopping-basket__footer-row  ">
+                        <div class="col-xs-4">
+                          <span class="">Sub total</span>
                         </div>
-
+                        <div class="col-xs-8  text-right">
+                          <span class="">
+                            <span class="">&dollar;</span>
+                            <span class="">2 550</span>
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                  </li>
-                <? endfor; ?>
 
+                      <div class="row  shopping-basket__footer-row  ">
+                        <div class="col-xs-4">
+                          <span class="">Total</span>
+                        </div>
+                        <div class="col-xs-8  text-right">
+                          <span class="">
+                            <span class="">&dollar;</span>
+                            <span class="">10 200</span>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-xs-6  ">
+                          <a class="links  btn  btn-default  theme-buttons__buttons--default" href="?page=shopping-cart">View cart</a>
+                        </div>
+                        <div class="col-xs-6  text-right  ">
+                          <a class="links  btn  btn-default  theme-buttons__buttons--default" href="#">Checkout</a>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+                  </div>
+                </li>
               </ul>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
 
-
       </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+    </div><!-- /.container -->
   </nav>
 </header>
