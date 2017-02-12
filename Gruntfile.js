bac68efb4f110786a84e75f26ae8b42f10d0699d
Alexandr Ja <mr-spinn@outlook.com>
2017-02-13 00:22:24 +0600

    refactor Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,103 @@
 module.exports = function(grunt) {
   require('time-grunt')(grunt);
-    
-  var vendor_styles_build_path        = 'build/css/vendor-styles.css';
-  var vendor_styles_min_build_path    = 'build/css/vendor-styles.min.css';
-  var vendor_styles_min_load_path     = 'css/vendor-styles.min.css';
-
-  var layout_styles_build_path        = 'build/css/layout-styles.css';
-  var layout_styles_min_build_path    = 'build/css/layout-styles.min.css';
-  var layout_styles_min_load_path     = 'css/layout-styles.min.css';
-
-  var layout_styles_source_path        = 'source/global_styles_components/collected_styles/layout.less';
-  var layout_styles_source_load_path   = 'source/global_styles_components/collected_styles/layout.css';
-
-  var vendor_scripts_build_path       = 'build/js/vendor-scripts.js';
-  var vendor_scripts_min_build_path   = 'build/js/vendor-scripts.min.js';
-  var vendor_scripts_min_load_path    = 'js/vendor-scripts.min.js';
-
-  var layout_scripts_source_path      = 'source/global_js_components/layout.js';
-  var layout_scripts_build_path       = 'build/js/layout-scripts.js';
-  var layout_scripts_min_build_path   = 'build/js/layout-scripts.min.js';
-  var layout_scripts_min_load_path    = 'js/layout-scripts.min.js';
-
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     clean: {
-      build:       ["build"],
-      source_less: [layout_styles_source_path],
-      source_js:   [layout_scripts_source_path]
+      build:       ['build'],
+      source_less: ['source/global_styles_components/collected_styles/**/*.*'],
+      source_js:   ['source/global_js_components/collected_scripts/**/*.*']
+    },
+
+    concat: {
+      options: {
+        separator: '\n /*SeparatoR*/ \n'
+      },
+      layout_styles: {
+        src: [
+          'source/global_styles_components/**/*.less',
+          'source/main_modules/**/*.less',
+          'source/other_modules/**/*.less'
+        ],
+        dest: 'source/global_styles_components/collected_styles/less/layout.less'
+      },
+      vendor_styles: {
+        src: [
+          'source/global_third_components/libs/normalize-css/normalize.css',
+          'source/global_third_components/frameworks/bootstrap/css/bootstrap.css',
+          'source/global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.css',
+          'source/global_third_components/libs/noUiSlider/distribute/nouislider.css'
+        ],
+        dest: 'source/global_styles_components/collected_styles/css/vendor.css'
+      },
+      layout_scripts: {
+        src: ['source/global_js_components/layout_inception.js', 'source/main_modules/**/*.js', 'source/other_modules/**/*.js', 'source/global_js_components/layout_completion.js'],
+        dest: 'source/global_js_components/collected_scripts/js/layout.js'
+      },
+      vendor_scripts: {
+        src: [
+          'source/global_third_components/libs/jquery/jquery.js',
+          'source/global_third_components/frameworks/bootstrap/js/bootstrap.js',
+          'source/global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.js',
+          'source/global_third_components/libs/noUiSlider/distribute/nouislider.js'
+        ],
+        dest: 'source/global_js_components/collected_scripts/js/vendor.js'
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+          paths: ['source']
+        },
+        files: {
+          'source/global_styles_components/collected_styles/css/layout.css': ['source/global_styles_components/collected_styles/less/layout.less']
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('autoprefixer')({ browsers:
+          'Chrome >= 52, ' +
+          'Edge >= 13, ' +
+          'ff >= 48, ' +
+          'ie >= 10, ' +
+          'Opera >= 38, ' +
+          'Safari >= 9, ' +
+          'iOS >=9, ' +
+          'ChromeAndroid >= 50, ' +
+          'UCAndroid >= 0, ' +
+          'Android >= 4.4, ' +
+          'ExplorerMobile >= 10, ' +
+          'iOS >= 9, ' +
+          'OperaMobile >= 0, ' +
+          ''
+          })
+        ]
+      },
+      main_styles: {
+        src: 'source/global_styles_components/collected_styles/css/layout.css'
+      }
+    },
+
+    cmq: {
+      style: {
+        files: {
+          'source/global_styles_components/collected_styles/css/layout.css': ['source/global_styles_components/collected_styles/css/layout.css']
+        }
+      }
+    },
+
+    csscomb: {
+      style: {
+        expand: true,
+        src: ['source/global_styles_components/collected_styles/css/layout.css']
+      }
     },
 
     copy: {
@@ -90,152 +158,90 @@ module.exports = function(grunt) {
               "index.php"
             ],
             dest: "build"
+          },
+          {
+            expand: true,
+            cwd: "source/global_styles_components/collected_styles/css",
+            src: [
+              "**/*.css"
+            ],
+            dest: "build/css"
+          },
+          {
+            expand: true,
+            cwd: "source/global_js_components/collected_scripts/js",
+            src: [
+              "**/*.js"
+            ],
+            dest: "build/js"
           }
         ]
       }
     },
 
-    concat: {
-      options: {
-        separator: '\n /*SeparatoR*/ \n'
-      },
-      layout_styles: {
-        src: [
-          'source/global_styles_components/**/*.less',
-          'source/main_modules/**/*.less',
-          'source/other_modules/**/*.less'
-        ],
-        dest: layout_styles_source_path
-      },
-      vendor_styles: {
-        src: [
-          'source/global_third_components/libs/normalize-css/normalize.css',
-          'source/global_third_components/frameworks/bootstrap/dist/css/bootstrap.css',
-          'source/global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.css'
-        ],
-        dest: vendor_styles_build_path
-      },
-      layout_scripts_source: {
-        src: ['source/global_js_components/layout_inception.js', 'source/main_modules/**/*.js', 'source/other_modules/**/*.js', 'source/global_js_components/layout_completion.js'],
-        dest: layout_scripts_source_path
-      },
-      layout_scripts_build: {
-        src: ['source/global_js_components/layout.js', 'source/main_modules/**/*.js', 'source/other_modules/**/*.js'],
-        dest: layout_scripts_build_path
-      },
-      vendor_scripts: {
-        src: [
-          'source/global_third_components/libs/jquery/jquery.js',
-          'source/global_third_components/frameworks/bootstrap/js/bootstrap.js',
-          'source/global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.js',
-        ],
-        dest: vendor_scripts_build_path
-      }
-    },
-
-    less: {
-      development: {
-        options: {
-          paths: ['source']
-        },
-        files: {
-          'source/global_styles_components/collected_styles/layout.css': 'source/global_styles_components/collected_styles/layout.less'
-        }
-      }
-    },
-
-    postcss: {
-      options: {
-        map: false,
-        processors: [
-          require('autoprefixer')({ browsers:
-            'Chrome >= 52, ' +
-            'Edge >= 13, ' +
-            'ff >= 48, ' +
-            'ie >= 10, ' +
-            'Opera >= 38, ' +
-            'Safari >= 9, ' +
-            'iOS >=9, ' +
-            'ChromeAndroid >= 50, ' +
-            'UCAndroid >= 0, ' +
-            'Android >= 4.4, ' +
-            'ExplorerMobile >= 10, ' +
-            'iOS >= 9, ' +
-            'OperaMobile >= 0, ' +
-            ''
-          })
-        ]
-      },
-      main_styles: {
-        src: layout_styles_build_path
-      }
-    },
-
-    cmq: {
-      style: {
-        files: {
-          layout_styles_build_path: [layout_styles_build_path]
-        }
-      }
-    },
-
-    csscomb: {
-      style: {
-        expand: true,
-        src: [layout_styles_build_path]
-      }
-    },
-
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1
-      },
-      build: {
-        files: {
-          vendor_styles_min_build_path: [vendor_styles_build_path],
-          layout_styles_min_build_path: [layout_styles_build_path]
-        }
-      }
-    },
-
-    uglify: {
-      build: {
-        files: {
-          vendor_scripts_min_build_path: [vendor_scripts_build_path],
-          layout_scripts_min_build_path: [layout_scripts_build_path]
-        }
-      }
-    },
-
-    imagemin: {
-      images: {
-        options: {
-          optimizationLevel: 3
-        },
-        files: [{
-          expand: true,
-          src: ['build/images/**/*.{png,jpg,gif,svg}']
-        }]
-      }
-    },
-
     replace: {
-      paths: {
+      path_pages_data: {
         options: {
           patterns: [
             {
               match: /global_pages_data/g,
               replacement: 'pages_data'
-            },
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+             'build/index.php',
+             'build/pages_data/**'
+            ]
+          }
+        ]
+      },
+      path_data_builders: {
+        options: {
+          patterns: [
             {
               match: /page_builders/g,
               replacement: 'pages'
-            },
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'build/index.php',
+              'build/pages/**'
+            ]
+          }
+        ]
+      },
+      path_pages_other_modules: {
+        options: {
+          patterns: [
             {
-              match: /other_modules/g,
-              replacement: 'pages_modules/other_modules'
-            },
+              match: /'other_modules/g,
+              replacement: '\'pages_modules/other_modules'
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'build/pages_data/**',
+              'build/index.php',
+              'build/pages/**',
+              'build/pages_modules/**'
+            ]
+          }
+        ]
+      },
+      path_pages_main_modules: {
+        options: {
+          patterns: [
             {
               match: /main_modules/g,
               replacement: 'pages_modules/main_modules'
@@ -246,10 +252,7 @@ module.exports = function(grunt) {
           {
             expand: true,
             src: [
-             'build/index.php',
-             'build/pages/**',
-             'build/pages_data/**',
-             'build/pages_modules/**'
+              'build/pages/**',
             ]
           }
         ]
@@ -258,8 +261,8 @@ module.exports = function(grunt) {
         options: {
           patterns: [
             {
-              match: /global_third_components\//g,
-              replacement: ''
+              match: /\/tea-shop\/source\/global_third_components\//g,
+              replacement: '../'
             }
           ]
         },
@@ -267,7 +270,7 @@ module.exports = function(grunt) {
           {
             expand: true,
             src: [
-              layout_styles_build_path
+              'build/css/layout.css'
             ]
           }
         ]
@@ -281,7 +284,7 @@ module.exports = function(grunt) {
             },
             {
               match: /<\/head>/,
-              replacement: '\t\<link rel="stylesheet" href='+ vendor_styles_min_load_path +'>\n\t\<link rel="stylesheet" href=' + layout_styles_min_load_path + '>\n<\/head>'
+              replacement: '\t\<link rel="stylesheet" href='+ 'css/vendor.min.css' +'>\n\t\<link rel="stylesheet" href=' + 'css/layout.min.css' + '>\n<\/head>'
             }
           ]
         },
@@ -303,7 +306,7 @@ module.exports = function(grunt) {
             },
             {
               match: /<\/body>/,
-              replacement: '\t\<script src='+ vendor_scripts_min_load_path + '></script>\n\t\t\<script src='+ layout_scripts_min_load_path +'></script>\n\t<\/body>'
+              replacement: '\t\<script src='+ 'js/vendor.min.js' + '></script>\n\t\t\<script src='+ 'js/layout.min.js' +'></script>\n\t<\/body>'
             }
           ]
         },
@@ -337,16 +340,49 @@ module.exports = function(grunt) {
       }
     },
 
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      build: {
+        files: {
+          'build/css/vendor.min.css': ['build/css/vendor.css'],
+          'build/css/layout.min.css': ['build/css/layout.css']
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'build/js/vendor.min.js': ['build/js/vendor.js'],
+          'build/js/layout.min.js': ['build/js/layout.js']
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ['build/images/**/*.{png,jpg,gif,svg}']
+        }]
+      }
+    },
+
     watch: {
       options: {
         livereload: true
       },
       source_files: {
         files: ['source/**/*.php', 'source/**/*.less'],
-        tasks: ['clean', 'concat:layout_styles', 'concat:layout_scripts_source', 'less:development']
+        tasks: ['clean', 'concat:layout_styles', 'concat:layout_scripts', 'less:development']
       }
     }
-
   });
 
   // Load the plugin that provides tasks.
@@ -356,16 +392,17 @@ module.exports = function(grunt) {
   // Default task(s).
   grunt.registerTask('default', [
     'clean',
-    'copy',
     'concat',
+    'less',
     'postcss:main_styles',
     'cmq',
     'csscomb',
+
+    'copy',
     'replace',
     'cssmin',
-    'uglify',
+    'uglify',    
     'imagemin'
   ]);
 
-  grunt.registerTask('watch-manual', ['clean', 'concat:layout_styles', 'concat:layout_scripts_source', 'less:development']);
 };
