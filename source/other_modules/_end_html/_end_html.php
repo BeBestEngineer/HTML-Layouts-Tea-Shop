e49a130ceca47287115d4a3d9e49ac9e8a9df3e6
Alexandr Ja <mr-spinn@outlook.com>
2017-02-13 00:19:09 +0600

    update _end module

--- source/other_modules/_end_html/_end_html.php
+++ source/other_modules/_end_html/_end_html.php
@@ -5,6 +5,6 @@
     <script src="global_third_components/libs/owl-carousel-1/owl-carousel/owl.carousel.js"></script>
     <script src="global_third_components/libs/noUiSlider/distribute/nouislider.js"></script>
     <!--layout's scripts-->
-    <script src="global_js_components/layout.js"></script>
+    <script src="global_js_components/collected_scripts/js/layout.js"></script>
   </body>
 </html>
